14fbaedbbc2e57ab8c883444f31b4ed22ca8e09c
Antonioisrooted <antoniommaina@gmail.com>
2020-05-14 12:35:38 +0300

    Postmeal.java

--- src/main/java/Restaurantservices/Postmeal.java
+++ src/main/java/Restaurantservices/Postmeal.java
@@ -1,29 +1,75 @@
 package Restaurantservices;
 
+import org.sql2o.Connection;
+
 import java.util.List;
+import java.util.Objects;
 
-public class Postmeal {
-    public Postmeal(String pilau, String s, String billy_ayiera) {
+public class Postmeal  extends Animal{
+    public static final String DATABASE_TYPE = "Post";
+
+    public  Postmeal(String name, String description, String cook){
+        this.name = name;
+        this.description = description;
+        this.cook = cook;
+        type = DATABASE_TYPE;
     }
 
-    public static List<Object> all() {
+    public String getDescriptio(){
+
+        return description;
+    }
+    public String getCook(){
+        return cook;
     }
 
-    public String getDescription() {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Postmeal animal = (Postmeal ) o;
+        return id == animal.id &&
+                description.equals(animal.description) &&
+                cook.equals(animal.cook) &&
+                name.equals(animal.name) &&
+                type.equals(animal.type);
     }
 
-    public String getName() {
+    @Override
+    public int hashCode() {
+        return Objects.hash(description, cook, name, id, type);
     }
 
-    public String getCook() {
+    public static List<Postmeal> all(){
+        String sql = "SELECT * FROM animals WHERE type='Post'";
+        try(Connection con = DB.sql2o.open()){
+            return con.createQuery(sql)
+                    .executeAndFetch(Postmeal.class);
+        }
     }
 
-    public void save() {
-    }
+    public void save(){
 
-    public long getId() {
+        try(Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO animals (name,type,health,age) VALUES (:name,:type,:description,:cook);";
+            this.id = (int) con.createQuery(sql,true)
+                    .addParameter("name",this.name)
+                    .addParameter("type",this.type)
+                    .addParameter("description",this.description)
+                    .addParameter("cook",this.cook)
+                    .executeUpdate()
+                    .getKey();
+        }
     }
-
-    public Object find(long id) {
+    public static Postmeal find(int id) {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM animals where id=:id";
+            Postmeal animal = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetchFirst(Postmeal.class);
+            return animal;
+        }
     }
 }
+
