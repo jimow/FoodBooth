35f266210fce6923379ed2e5518f0f2411857b54
Antonioisrooted <antoniommaina@gmail.com>
2020-05-14 06:54:54 +0300

    PostmealTest.java file

---
+++ src/main/java/Postmeal.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+public class Postmeal {
+    public Postmeal(String pilau, String s, String billy_ayiera) {
+    }
+
+    public static List<Object> all() {
+    }
+
+    public String getDescription() {
+    }
+
+    public String getName() {
+    }
+
+    public String getCook() {
+    }
+
+    public void save() {
+    }
+
+    public long getId() {
+    }
+
+    public Object find(long id) {
+    }
+}
